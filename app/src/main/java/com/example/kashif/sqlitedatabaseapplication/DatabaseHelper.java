6bcc68b4439b1ac65d3f23a08d26b80f24fc8ea5
daniyalahmedkhan <daniyalahmed520@gmail.com>
2018-06-25 23:10:20 +0500

    Android Internal Local Storage Database SQLite Code Create DB

---
+++ app/src/main/java/com/example/kashif/sqlitedatabaseapplication/DatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.kashif.sqlitedatabaseapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public  static  final  String DATABASE_NAME = "Student.db";
+    public  static  final  String TABLE_NAME  = "student_table";
+
+
+    public  static  final  String COL_1  = "ID";
+    public  static  final  String COL_2  = "NAME";
+    public  static  final  String COL_3  = "SURNAME";
+    public  static  final  String COL_4  = "MARKS";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME , null , 1);
+        SQLiteDatabase database = this.getWritableDatabase();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT ,NAME TEXT ,SURNAME TEXT,MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/example/kashif/sqlitedatabaseapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.kashif.sqlitedatabaseapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseHelper = new DatabaseHelper(this);
+
+    }
+}
